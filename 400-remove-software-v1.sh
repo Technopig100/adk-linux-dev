345dfc49d8f1129e0f2945be93c252a4813a1ce2
jacolinux <technopig@e.email>
2022-02-16 14:19:50 -0500

    K

--- 400-remove-software-v1.sh
+++ 400-remove-software-v1.sh
@@ -16,13 +16,19 @@ sudo pacman -Rs broadcom-wl-dkms --noconfirm
 
 sudo pacman -Rs r8168-dkms --noconfirm
 
-sudo pacman -Rs xf86-video-amdgpu --noconfirm
+#sudo pacman -Rs xf86-video-amdgpu --noconfirm
 sudo pacman -Rs xf86-video-fbdev --noconfirm
 sudo pacman -Rs xf86-video-openchrome --noconfirm
 sudo pacman -Rs xf86-video-vmware --noconfirm
 sudo pacman -Rs xf86-video-ati --noconfirm
 sudo pacman -Rs xf86-video-nouveau --noconfirm
 sudo pacman -Rs xf86-video-vesa --noconfirm
+sudo pacman -Rs intel-ucode --noconfirm
+#sudo pacman -Rs amd-ucode --noconfirm
+sudo pacman -Rs virtualbox-guest-utils --noconfirm
+sudo pacman -Rs spice-vdagent --noconfirm
+sudo pacman -Rs xf86-input-synaptics --noconfirm
+#sudo pacman -Rs  --noconfirm
 
 echo "################################################################"
 echo "################### software removed"
